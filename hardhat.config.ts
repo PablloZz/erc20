89e80135fcc2bbe669aa1c9e97291a5bd393d6c0
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-06-26 12:10:30 +0300

    Commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -43,7 +43,7 @@ const config: HardhatUserConfig = {
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   mocha: {
     timeout: 500000,
